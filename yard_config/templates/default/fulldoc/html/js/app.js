4aafbd595fbdf522fdc3ea5a76b72cff7725ca52
Elliot Winkler <elliot.winkler@gmail.com>
2014-01-31 10:54:22 -0700

    Clean up some things

--- yard_config/templates/default/fulldoc/html/js/app.js
+++ yard_config/templates/default/fulldoc/html/js/app.js
@@ -1,51 +1,45 @@
 function createSourceLinks() {
-    $('.method_details_list .source_code').
-        before("<span class='showSource'>[<a href='#' class='toggleSource'>View source</a>]</span>");
-    $('.toggleSource').toggle(function() {
-       $(this).parent().nextAll('.source_code').show();
-       $(this).text("Hide source");
-    },
-    function() {
-        $(this).parent().nextAll('.source_code').hide();
-        $(this).text("View source");
-    });
+  $('.method_details_list .source_code').
+    before("<span class='showSource'>[<a href='#' class='toggleSource'>View source</a>]</span>");
+  $('.toggleSource').toggle(function() {
+    $(this).parent().nextAll('.source_code').show();
+    $(this).text("Hide source");
+  },
+  function() {
+    $(this).parent().nextAll('.source_code').hide();
+    $(this).text("View source");
+  });
 }
 
 function createDefineLinks() {
-    var tHeight = 0;
-    $('.defines').after(" <a href='#' class='toggleDefines'>more...</a>");
-    $('.toggleDefines').toggle(function() {
-        tHeight = $(this).parent().prev().height();
-        $(this).prev().show();
-        $(this).parent().prev().height($(this).parent().height());
-        $(this).text("(less)");
-    },
-    function() {
-        $(this).prev().hide();
-        $(this).parent().prev().height(tHeight);
-        $(this).text("more...");
-    });
+  var tHeight = 0;
+  $('.defines').after(" <a href='#' class='toggleDefines'>more...</a>");
+  $('.toggleDefines').toggle(function() {
+    tHeight = $(this).parent().prev().height();
+    $(this).prev().show();
+    $(this).parent().prev().height($(this).parent().height());
+    $(this).text("(less)");
+  },
+  function() {
+    $(this).prev().hide();
+    $(this).parent().prev().height(tHeight);
+    $(this).text("more...");
+  });
 }
 
 function createFullTreeLinks() {
-    var tHeight = 0;
-    $('.inheritanceTree').toggle(function() {
-        tHeight = $(this).parent().prev().height();
-        $(this).parent().toggleClass('showAll');
-        $(this).text("(hide)");
-        $(this).parent().prev().height($(this).parent().height());
-    },
-    function() {
-        $(this).parent().toggleClass('showAll');
-        $(this).parent().prev().height(tHeight);
-        $(this).text("show all");
-    });
-}
-
-function fixBoxInfoHeights() {
-    $('dl.box dd.r1, dl.box dd.r2').each(function() {
-       $(this).prev().height($(this).height());
-    });
+  var tHeight = 0;
+  $('.inheritanceTree').toggle(function() {
+    tHeight = $(this).parent().prev().height();
+    $(this).parent().toggleClass('showAll');
+    $(this).text("(hide)");
+    $(this).parent().prev().height($(this).parent().height());
+  },
+  function() {
+    $(this).parent().toggleClass('showAll');
+    $(this).parent().prev().height(tHeight);
+    $(this).text("show all");
+  });
 }
 
 function hideSearchFrameOnBlur() {
@@ -137,11 +131,15 @@ function framesInit() {
 
 function keyboardShortcuts() {
   if (window.top.frames.main) return;
+
   $(document).keypress(function(evt) {
     if (evt.altKey || evt.ctrlKey || evt.metaKey || evt.shiftKey) return;
-    if (typeof evt.target !== "undefined" &&
-        (evt.target.nodeName == "INPUT" ||
-        evt.target.nodeName == "TEXTAREA")) return;
+
+    if (
+      typeof evt.target !== "undefined" &&
+      (evt.target.nodeName == "INPUT" || evt.target.nodeName == "TEXTAREA")
+    ) return;
+
     switch (evt.charCode) {
       case 67: case 99:  $('#class_list_link').click(); break;  // 'c'
       case 77: case 109: $('#method_list_link').click(); break; // 'm'
@@ -255,7 +253,6 @@ $(framesInit);
 $(createSourceLinks);
 $(createDefineLinks);
 $(createFullTreeLinks);
-//$(fixBoxInfoHeights);
 $(initSearchFrameLinks);
 $(linkSummaries);
 $(keyboardShortcuts);
